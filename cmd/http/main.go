0d1f9401b912cfb5cbb3fc2ecbd71c0a1d8ba8eb
emanuel3k <emanuelsantos_677@hotmail.com>
2025-03-14 16:47:29 -0300

    feat: start function

--- cmd/http/main.go
+++ cmd/http/main.go
@@ -1,5 +1,18 @@
 package main
 
-func main() {
+import (
+	"fmt"
+	"github.com/emanuel3k/product-golang/cmd/http/routes"
+	"log"
+	"net/http"
+)
 
+func main() {
+	r := routes.NewRouter()
+
+	var port = ":8080"
+	fmt.Println("Server started on http://localhost:8080 🚀")
+	if err := http.ListenAndServe(port, r.MapRoutes()); err != nil {
+		log.Fatal(err)
+	}
 }
