5a650f3fa6b893ded560b0dd96b290c20f7580fc
emanuel3k <emanuelsantos_677@hotmail.com>
2025-03-14 16:47:10 -0300

    feat: product json repository config

--- internal/domain/product/productJsonRepository.go
+++ internal/domain/product/productJsonRepository.go
@@ -1,6 +1,22 @@
 package product
 
+import (
+	"github.com/emanuel3k/product-golang/storage"
+)
+
 const path = "./storage/json/products.json"
 
 type productJsonRepository struct {
+	products []*Product
+}
+
+func (pjr productJsonRepository) GetAll() ([]*Product, error) {
+	var err error
+	pjr.products, err = storage.ReadJson[Product](path)
+
+	if err != nil {
+		// todo
+	}
+
+	return pjr.products, nil
 }
