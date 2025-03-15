399a2aafb66f010fa69b78268eac122473b8a8ea
emanuel3k <emanuelsantos_677@hotmail.com>
2025-03-15 18:19:49 -0300

    feat: create product and get by code value

--- internal/domain/product/product.go
+++ internal/domain/product/product.go
@@ -10,12 +10,35 @@ type Product struct {
 	Price       float64 `json:"price"`
 }
 
+type BodyRequest struct {
+	Name        string  `json:"name" validate:"required"`
+	Quantity    int     `json:"quantity" validate:"required"`
+	CodeValue   string  `json:"codeValue" validate:"required"`
+	IsPublished bool    `json:"isPublished"`
+	Expiration  string  `json:"expiration" validate:"required"`
+	Price       float64 `json:"price" validate:"required"`
+}
+
+func (br *BodyRequest) toDomain() Product {
+	return Product{
+		Name:        br.Name,
+		Quantity:    br.Quantity,
+		CodeValue:   br.CodeValue,
+		IsPublished: br.IsPublished,
+		Expiration:  br.Expiration,
+		Price:       br.Price,
+	}
+}
+
 type IService interface {
 	GetAll() ([]*Product, error)
 	GetById(productId int) (*Product, error)
+	Create(body BodyRequest) (*Product, error)
 }
 
 type IRepository interface {
 	GetAll() ([]*Product, error)
 	GetById(productId int) (*Product, error)
+	Create(p *Product) error
+	GetByCodeValue(codeValue string) (*Product, error)
 }
--- internal/domain/product/productJsonRepository.go
+++ internal/domain/product/productJsonRepository.go
@@ -11,10 +11,11 @@ type productJsonRepository struct {
 }
 
 func (pjr *productJsonRepository) GetAll() ([]*Product, error) {
-	pjr.loadProducts()
+	if err := pjr.loadProducts(); err != nil {
+		// todo
+	}
 
 	products := make([]*Product, 0, len(pjr.products))
-
 	for _, p := range pjr.products {
 		products = append(products, p)
 	}
@@ -23,11 +24,46 @@ func (pjr *productJsonRepository) GetAll() ([]*Product, error) {
 }
 
 func (pjr *productJsonRepository) GetById(productId int) (*Product, error) {
+	if err := pjr.loadProducts(); err != nil {
+		// todo
+	}
 	return pjr.products[productId], nil
 }
 
-func (pjr *productJsonRepository) loadProducts() {
+func (pjr *productJsonRepository) GetByCodeValue(codeValue string) (*Product, error) {
+	if err := pjr.loadProducts(); err != nil {
+		// todo
+	}
 
+	for _, p := range pjr.products {
+		if p.CodeValue == codeValue {
+			return p, nil
+		}
+	}
+
+	return nil, nil
+}
+func (pjr *productJsonRepository) Create(request *Product) error {
+	if err := pjr.loadProducts(); err != nil {
+		// todo
+	}
+
+	request.ID = len(pjr.products) + 1
+	pjr.products[request.ID] = request
+
+	products := make([]*Product, 0, len(pjr.products))
+	for _, p := range pjr.products {
+		products = append(products, p)
+	}
+
+	if err := storage.WriteJson(path, products); err != nil {
+		// todo
+	}
+
+	return nil
+}
+
+func (pjr *productJsonRepository) loadProducts() error {
 	tmp, err := storage.ReadJson[Product](path)
 	if err != nil {
 		// todo
@@ -38,4 +74,5 @@ func (pjr *productJsonRepository) loadProducts() {
 		pjr.products[p.ID] = p
 	}
 
+	return nil
 }
