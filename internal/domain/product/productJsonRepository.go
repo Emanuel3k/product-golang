5b4d938ff10d5517cd9c53bebf6f4201942edfb6
emanuel3k <emanuelsantos_677@hotmail.com>
2025-03-15 14:44:40 -0300

    feat: func for load products and get by id

--- internal/domain/product/productJsonRepository.go
+++ internal/domain/product/productJsonRepository.go
@@ -7,16 +7,35 @@ import (
 const path = "./storage/json/products.json"
 
 type productJsonRepository struct {
-	products []*Product
+	products map[int]*Product
 }
 
-func (pjr productJsonRepository) GetAll() ([]*Product, error) {
-	var err error
-	pjr.products, err = storage.ReadJson[Product](path)
+func (pjr *productJsonRepository) GetAll() ([]*Product, error) {
+	pjr.loadProducts()
 
+	products := make([]*Product, 0, len(pjr.products))
+
+	for _, p := range pjr.products {
+		products = append(products, p)
+	}
+
+	return products, nil
+}
+
+func (pjr *productJsonRepository) GetById(productId int) (*Product, error) {
+	return pjr.products[productId], nil
+}
+
+func (pjr *productJsonRepository) loadProducts() {
+
+	tmp, err := storage.ReadJson[Product](path)
 	if err != nil {
 		// todo
 	}
 
-	return pjr.products, nil
+	pjr.products = make(map[int]*Product)
+	for _, p := range tmp {
+		pjr.products[p.ID] = p
+	}
+
 }
