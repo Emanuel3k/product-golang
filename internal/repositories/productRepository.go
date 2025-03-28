4dca2c0aaca66d261ee8de66cacbfcad1c00a644
emanuel3k <emanuelsantos_677@hotmail.com>
2025-03-28 17:49:46 -0300

    feat: update repository initialization to use PostgreSQL connection

--- internal/repositories/productRepository.go
+++ internal/repositories/productRepository.go
@@ -1,10 +1,13 @@
 package repositories
 
 import (
+	"database/sql"
 	"github.com/emanuel3k/product-golang/internal/domain"
 	"github.com/emanuel3k/product-golang/internal/domain/product"
 )
 
-func NewRepository() domain.IRepository {
-	return &product.JsonRepository{}
+func NewRepository(postgres *sql.DB) domain.IRepository {
+	return &product.PostgresRepository{
+		Conn: postgres,
+	}
 }
